dd2beac7fbee9442303cc7d9cc2815fee5682141
Woonayoung(dn225) <dn225@naver.com>
2020-10-20 12:21:57 +0900

    나영 삭제중

--- src/_reducers/index.js
+++ src/_reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from "redux";
 import member from "./user_reducer";
 import posts from "./post_reducer";
 import galleries from "./gallery_reducer";
-import position from "./search_reducer";
+// import position from "./search_reducer";
 import weather from "./weather_reducer";
 
 const rootReducer = combineReducers({
